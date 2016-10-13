f4156065d8af6a04fd2ef6f763304565f5143a3b
Edward Chen <edwardchen805@gmail.com>
2016-10-12 19:06:45 -0700

    Adding projects
    //TODO: Rename Program - Workingv2 to whatever the file is doing
    Brackets - checks a string for open/close brackets, paren and curly braces. returns the index of the string if one is not closed
    JobQueue - practicing parallel processing using priority queues

---
+++ Brackets.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Brackets;
+
+namespace Brackets
+{    
+    public class Brackets
+    {
+        char type;
+        int position;
+
+        Brackets(char _type, int _position)
+        {
+            type = _type;
+            position = _position;
+        }
+
+        public Brackets()
+        {
+        }
+
+        public int GetPosition()
+        {
+            return this.position;
+        }
+
+        public Boolean Match(char b)
+        {
+            if (this.type == '[' && b == ']')
+                return true;
+            if (this.type == '{' && b == '}')
+                return true;
+            if (this.type == '(' && b == ')')
+                return true;
+            return false;
+        }        
+
+        static bool isBalancev5(string input)
+        {
+            Stack<Brackets> open_tag = new Stack<Brackets>();
+            Stack<Brackets> close_tag = new Stack<Brackets>();
+            char c;
+
+
+            if (input.Length == 1)
+            {
+                Console.WriteLine(1);
+                return false;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                c = input[i];
+                Brackets open_temp = new Brackets();                
+
+                // Processes open tags
+                if (c == '(' || c == '[' || c == '{')
+                {                    
+                    open_temp.type = c;
+                    open_temp.position = i+1;
+                    open_tag.Push(open_temp);
+                }
+
+                // Processes close tags
+                if (c == ')' || c == ']' || c == '}')
+                {
+                    if (open_tag.Count == 0)
+                    {
+                        Console.WriteLine(i+1);
+                        return false;
+                    }                  
+
+                    if (open_tag.Peek().Match(c))
+                    {
+                        open_tag.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine(i+1);
+                        return false;
+                    }
+                }                                
+            }
+
+            if (open_tag.Count > 0)
+            {
+                Console.WriteLine(open_tag.Peek().position);
+                return false;
+            }
+
+            return true;
+        }
+
+        // End of class
+
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();                        
+
+            bool isSuccess = isBalancev5(input);
+            if (isSuccess)
+                Console.WriteLine("Success");
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
---
+++ Program - Workingv2'.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapBuild
+{
+    public class Program
+    {
+        public static int[] data;
+        public static List<Swap> swaps;
+
+        public class Swap
+        {
+            public int index1;
+            public int index2;
+
+            public Swap(int node1, int node2)
+            {
+                index1 = node1;
+                index2 = node2;
+            }
+        }
+
+        
+
+        
+        
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            data = new int[Int32.Parse(input)];
+
+            string stringInput = Console.ReadLine();
+            string[] strArray = stringInput.Split(' ');
+            strArray = strArray.Where(s => !String.IsNullOrEmpty(s)).ToArray(); // removes any nulls or empty indexes
+            data = Array.ConvertAll<string, int>(strArray, int.Parse);
+            
+
+            BuildHeap(data);
+            
+            Console.ReadKey();
+        }
+
+        public static void BuildHeap(int[] array)
+        {
+            int size = array.Count()-1;
+            swaps = new List<Swap>();
+
+            for (int i = (size-1/2); i>=0; i--)
+            {
+                if ((2 * i + 1) >= size)
+                    continue;
+                else
+                    SiftDown(i);
+            }
+
+            
+            Console.WriteLine(swaps.Count);
+            foreach(Swap val in swaps)
+            {
+                Console.WriteLine(val.index1 + " " + val.index2);
+            }
+        }        
+
+        public static void SiftDown(int i)
+        {            
+            int maxindex = i;
+            int l;
+            int r;
+
+            l = 2 * i +1;
+            r = 2 * i + 2;
+            
+            // if the left child is within the tree and the parent is higher than left child
+            if (l <= data.Count() && data[l] <= data[maxindex])
+            {
+                maxindex = l;
+            }                       
+            // if the right child is within the tree and the parent is higher than right child
+            if (r <= data.Count() && data[r] <= data[maxindex])
+            {
+                maxindex = r;
+            }
+
+            if(i != maxindex)
+            {
+                swaps.Add(new Swap(i, maxindex));
+                swapNumbers(ref data[i], ref data[maxindex]);
+                SiftDown(maxindex);
+            }
+            
+        }
+
+        public static void swapNumbers(ref int a, ref int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+
+    }
+}
